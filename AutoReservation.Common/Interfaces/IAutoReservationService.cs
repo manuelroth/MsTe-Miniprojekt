8c3cd4160aced1e4e132937db527c5ac1195cd41
Manuel Roth <manuelroth@hotmail.ch>
2015-11-24 20:03:12 +0100

    Implement AutoServiceInterface

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,7 +1,66 @@
-﻿namespace AutoReservation.Common.Interfaces
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using AutoReservation.Common.DataTransferObjects;
+
+namespace AutoReservation.Common.Interfaces
 {
+
+    [ServiceContract]
     public interface IAutoReservationService
     {
-        
+
+        // CRUD Autos
+        [OperationContract]
+        List<AutoDto> getAutos();
+
+        [OperationContract]
+        AutoDto getAuto(int index);
+
+        [OperationContract]
+        AutoDto addAuto(AutoDto auto);
+
+        [OperationContract]
+        [FaultContract(typeof(UpdateConcurrencyFault))]
+        void updateAuto(AutoDto modified, AutoDto original);
+
+        [OperationContract]
+        void deleteAuto(AutoDto auto);
+
+
+        // CRUD Kunden
+        [OperationContract]
+        List<KundeDto> getKunden();
+
+        [OperationContract]
+        KundeDto getKunde(int index);
+
+        [OperationContract]
+        KundeDto addKunde(KundeDto kunde);
+
+        [OperationContract]
+        [FaultContract(typeof(UpdateConcurrencyFault))]
+        void updateKunde(KundeDto modified, KundeDto original);
+
+        [OperationContract]
+        void deleteKunde(KundeDto kunde);
+
+
+        // CRUD Reservation
+        [OperationContract]
+        List<ReservationDto> getReservations();
+
+        [OperationContract]
+        ReservationDto getReservation(int index);
+
+        [OperationContract]
+        ReservationDto addReservation(ReservationDto reservation);
+
+        [OperationContract]
+        [FaultContract(typeof(UpdateConcurrencyFault))]
+        void updateReservation(ReservationDto modified, ReservationDto original);
+
+        [OperationContract]
+        void deleteReservation(ReservationDto reservation);
+
     }
 }
